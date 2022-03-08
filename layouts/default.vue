66a91fc9b57df2f06aaf195c9a0fc9b585c36a08
Mika <mika.griebsch@gmail.com>
2022-03-08 11:52:22 +0100

    optik

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,30 +9,32 @@
 </template>
 
 <script>
+import Milchkaffe from "../components/Milchkaffe.vue"
 export default {
-  name: 'DefaultLayout',
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
+    name: "DefaultLayout",
+    data() {
+        return {
+            clipped: false,
+            drawer: false,
+            fixed: false,
+            items: [
+                {
+                    icon: "mdi-apps",
+                    title: "Welcome",
+                    to: "/"
+                },
+                {
+                    icon: "mdi-chart-bubble",
+                    title: "Inspire",
+                    to: "/inspire"
+                }
+            ],
+            miniVariant: false,
+            right: true,
+            rightDrawer: false,
+            title: "Vuetify.js"
+        };
+    },
+    components: { Milchkaffe }
 }
 </script>
