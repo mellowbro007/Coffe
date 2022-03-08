127494f1157512fc1abdffe44fe16f01e1381aa6
Mika <mika.griebsch@gmail.com>
2022-03-08 11:57:56 +0100

    unnötiger kram aus defalt.vue entfertn

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,28 +11,8 @@
 <script>
 import Milchkaffe from "../components/Milchkaffe.vue"
 export default {
-    name: "DefaultLayout",
     data() {
         return {
-            clipped: false,
-            drawer: false,
-            fixed: false,
-            items: [
-                {
-                    icon: "mdi-apps",
-                    title: "Welcome",
-                    to: "/"
-                },
-                {
-                    icon: "mdi-chart-bubble",
-                    title: "Inspire",
-                    to: "/inspire"
-                }
-            ],
-            miniVariant: false,
-            right: true,
-            rightDrawer: false,
-            title: "Vuetify.js"
         };
     },
     components: { Milchkaffe }
